8411d2e4303919771c06d861817cf30ff0edd68f
Arlei F. Farnetani Junior <farnetani@gmail.com>
2025-09-21 18:54:20 -0300

    fix: corrigir sintaxe do prettierrc e configuração do tailwind css v4

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import Tasks from './Tasks'
 
 function App() {
     return (
+        <>
+        <h1 className="font-poppins font-bold text-2xl">App Todo</h1> 
         <Tasks />
+        </>
     )
 
 }
